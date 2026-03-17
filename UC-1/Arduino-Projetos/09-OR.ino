bbe7a3f4ba2e2b789763aefb870d94abcab108df
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-03-17 11:30:03 -0300

    Create 09-OR.ino

---
+++ UC-1/Arduino-Projetos/09-OR.ino
@@ -0,0 +1,27 @@
+/**
+  Funções lógicas - OR
+  Simulador de uma prensa bi-manual
+  @author Anderson Wilmer
+  Data: 16 de setembro de 2025
+*/
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  digitalWrite(13, HIGH);
+
+}
+
+void loop() {
+  int botao1 = digitalRead(2);
+  int botao2 = digitalRead(3);
+  //Função OR
+  //Se o botao1 "OU" o botao2 for pressionado
+  if (botao1 == 0 || botao2 == 0) {
+    digitalWrite(13, LOW);
+  } else {
+    digitalWrite(13, HIGH);
+  }
+
+}
