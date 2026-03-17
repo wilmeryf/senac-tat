e60b44476fc283aa6baecb33dc8ad5a1f2c00c81
Anderson Wilmer <anderson.wilmer2016@gmail.com>
2026-03-17 11:51:29 -0300

    Create 15-While-e-Do-While.ino

---
+++ UC-1/Arduino-Projetos/15-While-e-Do-While.ino
@@ -0,0 +1,39 @@
+/**
+  Estudo da estruturas de repetição while | do...while
+  @author Anderson Wilmer
+  Data: 18 de setembro de 2025
+  Sintaxe:
+
+  while (condição) {
+    //código será repetido enquanto condição verdadeira
+  }
+
+  do {
+    //código é executado 1 vez independente da condição
+    //depois a condição é analisada e o código será repetido
+    //enquanto condição verdadeira
+  } while (condição)
+*/
+
+void setup() {
+  Serial.begin(9600);
+  //nas estrutura while | do...while não é possível inicializar uma variável como no "for"
+  int i = 100; //inicialização (declaração) da variável
+  while (i < 10) {
+    Serial.print("i = ");
+    Serial.println(i);
+    i++;
+  }
+
+  int j = 100;
+  do {
+    Serial.print("j = ");
+    Serial.println(j);
+    j++;
+  } while(j < 10);
+
+}
+
+void loop() {
+
+}
